c056b8e9af9e34a98d6aed145c9fad8f588ccafd
Aleksey Tkachenko <alexei.tkch@gmail.com>
2023-11-28 09:10:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dogs/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.dogs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private ImageView imageViewDog;
+    private ProgressBar progressBar;
+    private Button buttonLoadImage;
+
+    private MainViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        viewModel.loadDogImage();
+
+        viewModel.getIsError().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean errorIs) {
+                if (errorIs) {
+                    Toast.makeText(
+                            MainActivity.this,
+                            R.string.error_loading,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        viewModel.getIsLoading().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean loading) {
+                if (loading) {
+                    progressBar.setVisibility(View.VISIBLE);
+                } else {
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        viewModel.getDogImage().observe(this, new Observer<DogImage>() {
+            @Override
+            public void onChanged(DogImage dogImage) {
+                Glide.with(MainActivity.this)
+                        .load(dogImage.getMessage())
+                        .into(imageViewDog);
+            }
+        });
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewModel.loadDogImage();
+            }
+        });
+    }
+
+    private void initViews() {
+        imageViewDog = findViewById(R.id.imageViewDog);
+        progressBar = findViewById(R.id.progressBar);
+        buttonLoadImage = findViewById(R.id.buttonLoadImage);
+    }
+}
